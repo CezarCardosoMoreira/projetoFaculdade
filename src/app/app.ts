24a931685fd8f3a32a802d1fc985adfcc71165f9
Cezar Moreira <cezargithub@gmail.com>
2025-07-23 20:50:17 -0300

    initial

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Olamundo } from "./olamundo/olamundo";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Olamundo],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/olamundo/olamundo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-olamundo',
+  imports: [],
+  templateUrl: './olamundo.html',
+  styleUrl: './olamundo.css'
+})
+export class Olamundo {
+
+}
